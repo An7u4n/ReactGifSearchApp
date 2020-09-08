a5439764526763ca53e95f5dc1b02b6a1c2c7035
Miguel Centurion <miguelitocentu@hotmail.com>
2020-09-08 20:52:33 -0300

    docs added

---
+++ docs/precache-manifest.c078e5bd5e533d5f80b67e0d7946e36b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb48ca8614cca389fcbd17df271f94d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "820583200d2dcf2a0026",
+    "url": "/static/css/main.e1fbc3ce.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820583200d2dcf2a0026",
+    "url": "/static/js/main.c4dfa998.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
